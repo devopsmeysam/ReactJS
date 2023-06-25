cd5ea8a1226bfcea718771df3c25fdaf3af6b5d4
devopsmeysam <91230909+devopsmeysam@users.noreply.github.com>
2023-06-25 19:17:43 -0400

    Plays around

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const name = "Meysam";
+  const isNameShowing = true;
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p>Hello {isNameShowing ? name : 'JANA'}!</p><p>Welcome to the React.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
-        <a
+        {isNameShowing ? (
+          <>test</>
+        ) : (
+          <>
+          <h1>Name doesn't showing</h1>
+          <h2>Good Luck</h2>
+          </>
+        )}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
